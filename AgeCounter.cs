d6c8eeed3f9c91749994599c897fec3d57186afd
charouzic <viki.macek@gmail.com>
2020-09-18 05:42:31 +0200

    Add files via upload

---
+++ AgeCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Macek_assignment1
+{
+    public class AgeCounter
+    {
+        //public string firstName;
+
+        public void GetMyInformation(string firstName, string lastName, string city, string country,DateTime dob)
+        {
+            Console.WriteLine($"Name: {firstName} {lastName}");
+            Console.WriteLine($" City: {city}");
+            Console.WriteLine($" Country: {country}");
+            Console.WriteLine($" Date of birth: {dob}");
+        }
+
+        public int DaysPassedSinceYourBirth(DateTime dob)
+        {
+            TimeSpan timeSpan = DateTime.Now - dob;
+            int totalDays = timeSpan.Days;
+            return totalDays;
+        }
+
+        public double HoursPassedSinceYourBirth(DateTime dob)
+        {
+            double timeSpanHours = (DateTime.Now - dob).TotalHours;
+            return timeSpanHours;
+        }
+
+        public double MinutesPassedSinceYourBirth(DateTime dob)
+        {
+            double timeSpanMinutes = (DateTime.Now - dob).TotalMinutes;
+            return timeSpanMinutes;
+        }
+
+        public double SecondsPassedSinceYourBirth(DateTime dob)
+        {
+            double timeSpanSeconds = (DateTime.Now - dob).TotalSeconds;
+            return timeSpanSeconds;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Macek_assignment1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your personal age calculator");
+            Console.WriteLine("Please provide me with your details");
+
+            Console.Write("First Name: ");
+            string firstName = (Console.ReadLine());
+
+            Console.Write("Last Name: ");
+            string lastName = (Console.ReadLine());
+
+            Console.Write("City: ");
+            string city = (Console.ReadLine());
+
+            Console.Write("Country: ");
+            string country = (Console.ReadLine());
+
+            Console.Write("Date of birth (DD-MM-YY): ");
+            DateTime dob = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
+
+
+            Console.WriteLine("*********** Here are your details ***********");
+
+
+            // calling different class
+            AgeCounter agecounter  = new AgeCounter();
+
+
+            agecounter.GetMyInformation(firstName, lastName, city, country, dob);
+
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("*********** Here are your numbers ***********");
+            
+            Console.WriteLine($"Your approximate age in days: {agecounter.DaysPassedSinceYourBirth(dob)}");
+
+            Console.WriteLine($"Your approximate age in hours: {agecounter.HoursPassedSinceYourBirth(dob)}");
+
+            Console.WriteLine($"Your approximate age in minutes: {agecounter.MinutesPassedSinceYourBirth(dob)}");
+
+            Console.WriteLine($"Your approximate age in seconds: {agecounter.SecondsPassedSinceYourBirth(dob)}");
+
+            Console.WriteLine("***************** Thank you *****************");
+        }
+    }
+
+}
